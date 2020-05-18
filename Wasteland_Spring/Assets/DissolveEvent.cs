729b57ae3cd037b987a6374398ccd1f86602093a
jaw006 <jaw006@ucsd.edu>
2020-05-17 19:13:49 -0700

    attempt to smooth interpolate beweteen values

--- Wasteland_Spring/Assets/DissolveEvent.cs
+++ Wasteland_Spring/Assets/DissolveEvent.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -12,9 +13,11 @@ public class DissolveEvent : MonoBehaviour
     // Shader string
     public string materialProperty = "RenderPerCent";
     // Current value
+    private float lastMaterialPropertyAmount= 0.0f;
     private float currentMaterialPropertyAmount= 0.0f;
     // How much the amount increments on trigger
     public float materialPropertyIncrement = 0.01f;
+    public float smoothTime = 0.25f;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +34,7 @@ public class DissolveEvent : MonoBehaviour
             Debug.LogError("Error: material assigned to mesh does not have required property" +  materialProperty);
         }
 
+        // Reset material property
         SetMaterialProperty(0.0f);
     }
 
@@ -42,11 +46,9 @@ public class DissolveEvent : MonoBehaviour
     public void IncrementMaterialProperty()
     {
         // Increment and clamp dissolveAmount
-        currentMaterialPropertyAmount+= materialPropertyIncrement;
+        lastMaterialPropertyAmount = currentMaterialPropertyAmount;
+        currentMaterialPropertyAmount += materialPropertyIncrement;
         ClampMaterialPropertyAmount();
-
-        // Set the material property
-        SetMaterialProperty(currentMaterialPropertyAmount);
     }
 
     private void SetMaterialProperty(float newAmount)
@@ -64,6 +66,15 @@ public class DissolveEvent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentMaterialPropertyAmount >= 0.0f && currentMaterialPropertyAmount < 1.0f)
+        {
+            float velocity = 0.0f;
+            // Smoothly interpolate between values
+            //            float matPropValue = Mathf.Lerp(currentMaterialPropertyAmount, 1.0f, 0.1f * Time.deltaTime);
+
+            float matPropValue = Mathf.SmoothDamp(currentMaterialPropertyAmount, 1.0f, ref velocity, smoothTime, Time.deltaTime);
+            lastMaterialPropertyAmount += velocity;
+            SetMaterialProperty(matPropValue);
+        }
     }
 }
