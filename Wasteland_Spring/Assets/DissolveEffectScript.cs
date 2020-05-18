be6fcf9e13025fee6a658a926de1e8db706f741f
jaw006 <jaw006@ucsd.edu>
2020-05-17 18:38:06 -0700

    add script to Trigger a UnityEvent on collider hit

---
+++ Wasteland_Spring/Assets/DissolveEffectScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SphereCollider))]
+public class DissolveEffectScript : MonoBehaviour
+{
+
+    public UnityEvent triggerEvent;
+
+    [Range(0.0f, 9.9f)]
+    public float RandomTriggerDelay = 0.5f;
+    public int TriggerEventThreshold = 1; // How many times a trigger needs to be collided with to trigger event
+    public bool TriggerOnlyOnce = true;
+    private int timesTriggered = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(triggerEvent == null)
+        {
+            Debug.LogError("Error: triggerEvent is null!");
+        }
+    }
+
+    bool IsTriggered()
+    {
+        return (timesTriggered >= TriggerEventThreshold);
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+
+        // Check if the progress counter is full
+        if(TriggerOnlyOnce && IsTriggered())
+        {
+            return;
+        }
+
+        // Increment progress counter
+        timesTriggered += 1;
+
+        // Check again after incrementing
+        if(IsTriggered())
+        {
+            // Trigger event
+            if(triggerEvent != null)
+            {
+                StartCoroutine("TriggerEventWithRandomOffset");
+            }
+        }
+    }
+
+    public void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    public void Reset()
+    {
+        timesTriggered = 0;
+        StopAllCoroutines();
+    }
+
+    IEnumerator TriggerEventWithRandomOffset()
+    {
+        yield return new WaitForSeconds(Random.Range(0.0f, RandomTriggerDelay));
+        triggerEvent.Invoke();    
+    }
+}
---
+++ Wasteland_Spring/Assets/DissolveEvent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class DissolveEvent : MonoBehaviour 
+{
+    private MeshRenderer mesh;
+    private Material dissolveMat;
+    private string materialProperty = "RenderPerCent";
+    private float dissolveAmount = 0.0f;
+    private float dissolveAmountIncrement = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        if(mesh == null)
+        {
+            Debug.LogError("Error: mesh null in DissolveEvent");
+        }
+
+        dissolveMat = mesh.sharedMaterial;
+        if(!dissolveMat.HasProperty(materialProperty))
+        {
+            Debug.LogError("Error: material assigned to mesh does not have required property" +  materialProperty);
+        }
+    }
+
+    private void Reset()
+    {
+        dissolveAmount = 0.0f;
+    }
+
+    public void IncrementDissolve()
+    {
+        // Increment and clamp dissolveAmount
+        dissolveAmount += dissolveAmountIncrement;
+        ClampDissolve();
+
+        // Set the material property
+        dissolveMat.SetFloat(materialProperty, dissolveAmount);
+    }
+
+    // Clamp the value of dissolveAmount to 0.0 <= x <= 1.0
+    private void ClampDissolve()
+    {
+        dissolveAmount = (dissolveAmount > 1.0f) ? 1.0f : dissolveAmount;
+        dissolveAmount = (dissolveAmount < 0.0f) ? 0.0f : dissolveAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
