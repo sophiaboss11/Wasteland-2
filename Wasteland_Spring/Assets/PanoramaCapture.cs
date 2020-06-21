e0b2ca64b44346a71eb36d545a8b1989e069806d
sophiaboss11 <bosssophiaboss@gmail.com>
2020-06-20 20:39:29 -0700

    panorama script

---
+++ Wasteland_Spring/Assets/PanoramaCapture.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanoramaCapture : MonoBehaviour
+{
+    public Camera targetCamera;
+    public RenderTexture cubeMapLeft;
+    public RenderTexture equirectRT;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Capture();
+    }
+
+
+    public void Capture()
+    {
+        targetCamera.RenderToCubemap(cubeMapLeft);
+        cubeMapLeft.ConvertToEquirect(equirectRT);
+        Save(equirectRT);
+    }
+
+    public void Save(RenderTexture rt)
+    {
+        Texture2D tex = new Texture2D(rt.width, rt.height);
+
+        RenderTexture.active = rt;
+        tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        RenderTexture.active = null;
+
+        byte[] bytes = tex.EncodeToJPG();
+
+        string path = Application.dataPath + "/Panorama" + ".jpg";
+
+        System.IO.File.WriteAllBytes(path, bytes);
+    }
+}
